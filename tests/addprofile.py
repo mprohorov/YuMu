265e8c6aa698b91c6909ca2c801c7691b5c76135
Max <Max>
2016-08-15 23:41:29 -0400

    Added function that writes rows to users table

--- tests/addprofile.py
+++ tests/addprofile.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, DateTime
 
 #Connection key
-s = "mysql+mysqldb://max:Gentech16@148.4.117.98:3306/yumu_db"
+s = "mysql+mysqldb://max:Gentech16@192.168.0.11:3306/yumu_db"
 engine = create_engine(s)
 
 #use  sqlalchemy to execute SQL commands
@@ -15,19 +15,27 @@ result = conn.execute("SELECT * FROM yumu_db.account_settings")
 conn = engine.connect()
 
 Base = declarative_base()
-Base.metadata.reflect(engine)
+Base.metadata.reflect(engine, extend_existing=True)
 
 class account(Base):
-    __tablename__ = 'account_settings'
+    __table__ = Base.metadata.tables['account_settings']
 
-    user_id = Column(Integer, primary_key=True)
-    email = Column(String)
-    phone_number = Column(Integer)
-    first_name = Column(String)
-    last_name = Column(String)
-    zipcode = Column(Integer)
-    createdOn = Column(DateTime)
+    def is_active(self):
+        return True
+    def get_id(self):
+        return self.email
+    def is_authenticated(self):
+        return self.authenticated
+    def is_anonymous(self):
+        return False;
 
+
+"""
 example = account(user_id=000001, email="bob@example.com", phone_number=1234567890,
-                  first_name='Bob', last_name='Smith', zipcode=11374)
-print(example.first_name, example.last_name)
+                  first_name='Bob', last_name='Smith', zipcode=11374, password='password')
+"""+
+def createAccount(email, phone, fname, lname, zip, pw):
+    act = account(email = email, phone_number = phone, first_name = fname, last_name = lname,
+                   zipcode = zip, password = pw)
+    return act
+
