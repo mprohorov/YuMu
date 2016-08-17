e754e57738154c7217f765e69fce60d5643d13b9
Max <Max>
2016-08-17 09:10:55 -0400

    Write event prefs finished

--- Legacy/takepref.py
+++ Legacy/takepref.py
@@ -3,4 +3,3 @@ from app import models
 def enterPrefs(minb, maxb, activity, radius, location):
     prefs = models.preferences(min_budget = minb, max_budget = maxb,
                                activity = activity, radius = radius, location = location)
-    return prefs
--- app/writeinputs.py
+++ app/writeinputs.py
@@ -1,9 +1,14 @@
 from app import models
 from app import dbconnect
 
-Base = models.Base
-
 def createAccount(email, phone, fname, lname, zip, pw):
     act = models.account(email = email, phone_number = phone, first_name = fname, last_name = lname,
                    zipcode = zip, password = pw)
     dbconnect.session.add(act)
+    dbconnect.session.commit()
+
+def enterPrefs(minb, maxb, activity, radius, location):
+    prefs = models.preferences(min_budget = minb, max_budget = maxb,
+                               activity = activity, radius = radius, location = location)
+    dbconnect.session.add(prefs)
+    dbconnect.session.commit()
